ca45c787661e20a301ee6a98f32fa18fa4f70c1d
Igor Gasparin de Vargas <133717907+igovargas@users.noreply.github.com>
2025-05-04 11:11:10 -0300

    Code to add score values
    - Created code to insert grades into the Score class using the readScores method.
    - Added command to call the readScores method for the main class (GradeAvarage).

--- src/GradeAvarage.java
+++ src/GradeAvarage.java
@@ -1,5 +1,5 @@
 public class GradeAvarage {
     public static void main(String[] args) {
-        
+        Scores.readScores();
     }
 }
--- src/Scores.java
+++ src/Scores.java
@@ -11,8 +11,20 @@ public class Scores {
 
         System.out.println("\nInsert the first note:");
         double firstNote = input.nextDouble();
+        System.out.println("The value of the first note is: " + firstNote);
 
-        
+        System.out.println("\nInsert the second note:");
+        double secondNote = input.nextDouble();
+        System.out.println("The value of the second note is: " + secondNote);
 
+        System.out.println("\nInsert the third note:");
+        double thirdNote = input.nextDouble();
+        System.out.println("The value of the third note is: " + thirdNote);
+
+        System.out.println("\nInsert the fourth note:");
+        double fourthNote = input.nextDouble();
+        System.out.println("The value of the fourth note is: " + fourthNote);
+
+        input.close();
     }
 }
