eb4a9193ed1055a4d2d33eb0ba7137564861cbae
Igor Gasparin de Vargas <133717907+igovargas@users.noreply.github.com>
2025-05-04 11:00:22 -0300

    Creation of base classes
    The base classes were created for use in the project.
    
    The classes created were:
    
    - Calculation
    - Score

---
+++ src/Scores.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Scores {
+
+    public static void readScores() {
+
+        System.out.println("Hello, this is a program to read grades and calculate your average.");
+        System.out.println("Let's enter 4 grades to calculate your average.");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nInsert the first note:");
+        double firstNote = input.nextDouble();
+
+        
+
+    }
+}
