57d49263cdb56176e671a4e8f92edc1c5e1edc15
Igor Gasparin de Vargas <133717907+igovargas@users.noreply.github.com>
2025-05-05 20:44:29 -0300

    Refactor variable names and add score validation logic
    - Renamed note variables from firstNote, secondNote, etc, to note1, note2, etc. for consistency;
    - Added validation to prevent invalid grade input (must be between -1 and 100);
    - Implemented logic to check whether the student passed, needs recovery, or failed;
    - Simplified setter usage by integrating validation directly in readScores().

--- src/Calculation.java
+++ src/Calculation.java
@@ -5,10 +5,25 @@ public class Calculation {
         studentScores.readScores();
 
         System.out.println("\nLet's calculate the average of the entered grades:");
-        System.out.println(studentScores.getFirstNote() + " + " + studentScores.getSecondNote() + " + " + studentScores.getThirdNote() + " + " + studentScores.getFourthNote() + " / 4");
+        System.out.println(studentScores.getNote1() + " + " + studentScores.getNote2() + " + " + studentScores.getNote3() + " + " + studentScores.getNote4() + " / 4");
 
-        double average = (studentScores.getFirstNote() + studentScores.getSecondNote() + studentScores.getThirdNote() + studentScores.getFourthNote()) / 4;
+        double average = (studentScores.getNote1() + studentScores.getNote2() + studentScores.getNote3() + studentScores.getNote4()) / 4;
 
         System.out.println("\nThe average is: " + average);
+
+        schoolAvarage(average);
+    }
+
+    public static void schoolAvarage(double average) {
+
+        if (average >= 70) {
+            System.out.println("You are approved!\n");
+        } else if (average < 70 && average >= 50) {
+            System.out.println("You need to take a make-up exam.\n");
+        } else {
+            System.out.println("You have failed.\n");
+        }
     }
 }
+
+
--- src/Scores.java
+++ src/Scores.java
@@ -2,41 +2,41 @@ import java.util.Scanner;
 
 public class Scores {
 
-    double firstNote;
-    double secondNote;
-    double thirdNote;
-    double fourthNote;
+    double note1;
+    double note2;
+    double note3;
+    double note4;
 
-    public double getFirstNote() {
-        return firstNote;
+    public double getNote1() {
+        return note1;
     }
 
-    public void setFirstNote(double firstNote) {
-        this.firstNote = firstNote;
+    public void setNote1(double note1) {
+        this.note1 = note1;
     }
 
-    public double getSecondNote() {
-        return secondNote;
+    public double getNote2() {
+        return note2;
     }
 
-    public void setSecondNote(double secondNote) {
-        this.secondNote = secondNote;
+    public void setNote2(double note2) {
+        this.note2 = note2;
     }
 
-    public double getThirdNote() {
-        return thirdNote;
+    public double getNote3() {
+        return note3;
     }
 
-    public void setThirdNote(double thirdNote) {
-        this.thirdNote = thirdNote;
+    public void setNote3(double note3) {
+        this.note3 = note3;
     }
 
-    public double getFourthNote() {
-        return fourthNote;
+    public double getNote4() {
+        return note4;
     }
 
-    public void setFourthNote(double fourthNote) {
-        this.fourthNote = fourthNote;
+    public void setNote4(double note4) {
+        this.note4 = note4;
     }
 
     public void readScores() {
@@ -46,23 +46,27 @@ public class Scores {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("\nInsert the first note:");
-        setFirstNote(input.nextDouble());
-        System.out.println("The value of the first note is: " + getFirstNote());
-
-        System.out.println("\nInsert the second note:");
-        setSecondNote(input.nextDouble());
-        System.out.println("The value of the second note is: " + getSecondNote());
-
-        System.out.println("\nInsert the third note:");
-        setThirdNote(input.nextDouble());
-        System.out.println("The value of the third note is: " + getThirdNote());
-
-        System.out.println("\nInsert the fourth note:");
-        setFourthNote(input.nextDouble());
-        System.out.println("The value of the fourth note is: " + getFourthNote());
+        setNote1(readValidNote(input, "first"));
+        setNote2(readValidNote(input, "second"));
+        setNote3(readValidNote(input, "third"));
+        setNote4(readValidNote(input, "fourth"));
 
         input.close();
+
+    }
+
+    private double readValidNote(Scanner input, String label) {
+        double note;
+        while (true) {
+            System.out.println("\nInsert the " + label + " note:");
+            note = input.nextDouble();
+            if (note >= -1 && note <= 100) {
+                System.out.println("The value of the " + label + " note is: " + note + ".");
+                return note;
+            } else {
+                System.out.println("Invalid grade. Please enter a value between -1 and 100.");
+            }
+        }
     }
 
     
